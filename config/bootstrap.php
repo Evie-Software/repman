b6995269c469adb2e6ce223e2d9ac3cf6979db42
Pete Bishop <9081809+PeteBishwhip@users.noreply.github.com>
2025-05-15 09:46:39 +0100

    Env vars

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,6 +4,10 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+// Workaround for missing env vars
+putenv('COMPOSER_HOME=/home/forge/composer.licensemy.app/.composer');
+putenv('HOME=/home/forge/composer.licensemy.app');
+
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php') && ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV']) {
